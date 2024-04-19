ebdfb8bbb7cbe47a1b90c77f43de8c4013f7e616
Aleksandr <skon1207@yandex.ru>
2024-04-19 09:39:08 +0400

    chore: delete comments

--- list/1.cpp
+++ list/1.cpp
@@ -6,70 +6,70 @@ struct list {
     list *prev;
 };
 
-void push (list *&h, list *&t, int x){ //вставка элемента в конец списка
-    list *r = new list; //создаем новый элемент
+void push (list *&h, list *&t, int x){ 
+    list *r = new list;
     r->inf = x;
-    r->next = NULL; //всегда последний
-    if (!h && !t){ //если список пуст
-        r->prev = NULL; //первый элемент
-    h = r; //это голова
+    r->next = NULL;
+    if (!h && !t){
+        r->prev = NULL; 
+    h = r;
     }
     else{
-        t->next = r; //r - следующий для хвоста
-        r->prev = t; //хвост - предыдущий для r
+        t->next = r;
+        r->prev = t;
     }
-    t = r; //r теперь хвост
+    t = r;
 }
 
-void print (list *&h, list *&t){ //печать элементов списка
-    list *p = h; //укзатель на голову
-    while (p){ //пока не дошли до конца списка
+void print (list *&h, list *&t){ 
+    list *p = h;
+    while (p){ 
     cout << p->inf << " ";
-    p = p->next; //переход к следующему элементу
+    p = p->next; 
     }
 }
-void insert_after(list *&h, list *&t, list *r, int y){ //вставляем после r
-    list *p = new list; //создаем новый элемент
+void insert_after(list *&h, list *&t, list *r, int y){ 
+    list *p = new list; 
     p->inf = y;
-    if (r == t){ //если вставляем после хвоста
-        p->next = NULL; //вставляемый эл-т - последний
-        p->prev = r; //вставляем после r
+    if (r == t){
+        p->next = NULL; 
+        p->prev = r; 
         r->next = p;
-        t = p; //теперь хвост - p
+        t = p; 
     }
-    else{ //вставляем в середину списка
-        r->next->prev = p; //для следующего за r эл-та предыдущий - p
-        p->next = r->next; //следующий за p - следующий за r
-        p->prev = r; //p вставляем после r
+    else{ 
+        r->next->prev = p;
+        p->next = r->next; 
+        p->prev = r; 
         r->next = p;
     }
 }
 
-int minn(list *&h, list *&t){ //печать элементов списка
-    list *p = h; //укзатель на голову
+int minn(list *&h, list *&t){ 
+    list *p = h;
     int min_ = INT_MAX;
-    while (p){ //пока не дошли до конца списка
+    while (p){ 
         if (p->inf < min_) min_ = p->inf;
         p = p->next;
     }
-    return min_; //возвращаем указатель
+    return min_; 
 }
 
-int even(list *&h, list *&t){ //печать элементов списка
-    list *p = h; //укзатель на голову
+int even(list *&h, list *&t){
+    list *p = h; 
     int e = INT_MIN;
-    while (p){ //пока не дошли до конца списка
+    while (p){ 
         if (!(p->inf & 1)) e = p->inf;
         p = p->next;
     }
-    return e; //возвращаем указатель
+    return e; 
 }
 
 void result(list *&h, list *&t) {
     int min_i = minn(h,t);
     int ev = even(h, t);
-    list *p = h; //укзатель на голову
-    while (p){ //пока не дошли до конца списка
+    list *p = h; 
+    while (p){ 
         if (p->inf == min_i)
             insert_after(h, t, p, ev);
         p = p->next;
@@ -87,19 +87,18 @@ void del_list(list* &h) {
 }
 int main(){
     int n, y;
-    //инициализируем список
     list *h = NULL;
     list *t = NULL;
     cout << "n = ";
-    cin >> n;//вод количества элементов
+    cin >> n;
     cout << "origin: ";
-    for (int i = 0; i < n; i++) { //ввод самих элементов
+    for (int i = 0; i < n; i++) {
         cin >> y;
-        push(h, t, y); //добавление элемента в стек
+        push(h, t, y); 
     }
     result(h, t);
     cout << "result: ";
-    print(h, t);//выводим полученный список
+    print(h, t);
     del_list(h);
     return 0;
 }
--- list/3.cpp
+++ list/3.cpp
@@ -6,49 +6,50 @@ struct list {
     list *prev;
 };
 
-void push (list *&h, list *&t, int x){ //вставка элемента в конец списка
-    list *r = new list; //создаем новый элемент
+void push (list *&h, list *&t, int x){
+    list *r = new list; 
     r->inf = x;
-    r->next = NULL; //всегда последний
-    if (!h && !t){ //если список пуст
-        r->prev = NULL; //первый элемент
-    h = r; //это голова
+    r->next = NULL; 
+    if (!h && !t){ 
+        r->prev = NULL; 
+    h = r; 
     }
     else{
-        t->next = r; //r - следующий для хвоста
-        r->prev = t; //хвост - предыдущий для r
+        t->next = r; 
+        r->prev = t; 
     }
-    t = r; //r теперь хвост
+    t = r; 
 }
 
-void print (list *&h, list *&t){ //печать элементов списка
-    list *p = h; //укзатель на голову
-    while (p){ //пока не дошли до конца списка
+void print (list *&h, list *&t){
+
+    list *p = h; 
+    while (p){
     cout << p->inf << " ";
-    p = p->next; //переход к следующему элементу
+    p = p->next; 
     }
 }
 void del_curr(list *&h, list *&t, list *r) {
-    if (r == h && r == t) //если в списке один элемент
-        h = t = NULL; //то список будет пустым
-    else if (r == h) { //если надо удалить первый элемент в списке
-        h = h->next; //то сдвигаем голову h на один элемент
+    if (r == h && r == t)
+        h = t = NULL;
+    else if (r == h) {
+        h = h->next;
         h->prev = NULL;
     }
-    else if (r == t) { //если удаляем последний элемент
-        t = t->prev; //сдвигаем хвост на один элемент влево
+    else if (r == t) { 
+        t = t->prev; 
         t->next = NULL;
     }
-    else {//случай когда удаляемый элемент в середине списка
+    else {
         r->next->prev = r->prev;
         r->prev->next = r->next;
     }
-    delete r; //удаляем r
+    delete r; 
 }
 
 void result(list *&h, list *&t){
-    list *p = h;//указатель на голову
-    while (p){ //пока не дошли до конца списка
+    list *p = h;
+    while (p){ 
         list *tmp_p = p->next;
         int sum_ = 0, c = 0;
         while (tmp_p){
@@ -79,19 +80,18 @@ void del_list(list* &h) {
 }
 int main(){
     int n, y;
-    //инициализируем список
     list *h = NULL;
     list *t = NULL;
     cout << "n = ";
-    cin >> n;//вод количества элементов
+    cin >> n;
     cout << "origin: ";
-    for (int i = 0; i < n; i++) { //ввод самих элементов
+    for (int i = 0; i < n; i++) { 
         cin >> y;
-        push(h, t, y); //добавление элемента в стек
+        push(h, t, y);
     }
     result(h, t);
     cout << "result: ";
-    print(h, t);//выводим полученный список
+    print(h, t);
     del_list(h);
     return 0;
 }
